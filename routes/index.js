c273cf176c0182af4ff3de8400e81ca8501a7f0a
Johnathon Staples <deepfatfried1942@gmail.com>
2026-01-19 13:18:28 -0700

    dont know what else to do

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/contacts', require('./contacts'))
+router.use('/contacts.json', require('./contacts.json'))
 
 module.exports = router;

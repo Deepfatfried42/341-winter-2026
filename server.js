c5623500fdccc6a3952d41947e338430ea528119
Johnathon Staples <deepfatfried1942@gmail.com>
2026-01-19 10:30:24 -0700

    lesson 2 personal project working

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
-const connectDB = require('./DB/connection');
+const bodyParser = require('body-parser');
+const connectDB = require('./db/connection');
+const mongodb = require('./db/connect');
 const app = express();
 
 connectDB();
 const port = 8080;
 
-app.use(express.json({extended:false}));
-app.use('/api/userModel', require('./Api/user'));
-app.use('/', require('./routes'));
+//app.use(express.json({extended:false}));
+//app.use('/api/userModel', require('./Api/user'));
+//app.use('/', require('./routes'));
 
-app.listen(process.env.port || port);
-console.log('Web Server is listening at port ' + (process.env.port || port));
+//app.listen(process.env.port || port);
+//console.log('Web Server is listening at port ' + (process.env.port || port));
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});

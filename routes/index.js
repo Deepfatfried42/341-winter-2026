11ec501dc4e4b080bf7ba4524b008b709e929aef
Johnathon Staples <deepfatfried1942@gmail.com>
2026-01-19 12:40:16 -0700

    copy and paste to see if it works in render

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const routes = require('express').Router();
-
-const lesson1controller = require('../controllers/lesson1');
-
-routes.get('/', lesson1controller.johnathonRoute);
-routes.get('/angela', lesson1controller.angelaRoute);
-
-
 
 router.use('/contacts', require('./contacts'))
 
-module.exports = router, routes;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const connectDB = require('./db/connection');
 const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
 const app = express();
-
-connectDB();
-const port = 8080;
-
-app.use(express.json({extended:false}));
-app.use('/api/userModel', require('./Api/user'));
-app.use('/', require('./routes'));
-
-//app.listen(process.env.port || port);
-//console.log('Web Server is listening at port ' + (process.env.port || port));
 
 app
   .use(bodyParser.json())

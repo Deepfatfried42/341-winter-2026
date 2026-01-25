fc567695067ad5fae5fda987c9b0df7d25074066
Johnathon Staples <deepfatfried1942@gmail.com>
2026-01-24 20:35:16 -0700

    week 3

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const connectDB = require('./DB/connection');
 const mongodb = require('./DB/connect');
+
+const port = process.env.PORT || 8080;
 const app = express();
-
-connectDB('./DB/connection');
-const port = 8080;
-
-app.use(express.json({extended:false}));
-app.use('/api/userModel', require('./Api/user'));
-app.use('/', require('./routes'));
-
-//app.listen(process.env.port || port);
-//console.log('Web Server is listening at port ' + (process.env.port || port));
 
 app
   .use(bodyParser.json())
@@ -22,7 +13,7 @@ app
   })
   .use('/', require('./routes'));
 
-mongodb.initDb((err, mongodb) => {
+mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
